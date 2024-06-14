30f03045a050203ebe2aac19b2ab878697d34543
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2024-06-15 01:57:04 +0530

    last commit

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,12 @@ const cors=require('cors')
 const connectdatabse=require('./database');
 
 app.use(exprees.json());
-app.use(cors());
+app.use(cors({
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+}
+));
 const books=require('./routes/bookstoreroute');
 
 app.use('/bookstore',books)
